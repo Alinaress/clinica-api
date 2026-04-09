e7509d85f7287b560cfb26f23084a41f785e2e99
Alinaress <gomez171001@gmail.com>
2026-04-08 20:57:21 -0600

    modificacion com updateOrCreate

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,20 +2,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-
+use App\Models\User;
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('users')->insert([
-            'nickname'   => 'admin',
-            'email'      => 'admin@clinica.com',
-            'password'   => Hash::make('password123'),
-            'id_rol'     => 1, // admin
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@clinica.com'],
+            [
+                'nickname' => 'admin',
+                'password' => Hash::make('password123'),
+                'id_rol' => 1,
+            ]
+        );
     }
 }
